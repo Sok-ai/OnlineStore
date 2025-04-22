beede7d295f9adbc8e4c926e3329b2c044098606
Sok-ai <Sokol152433@gmail.com>
2025-04-22 18:18:27 +0300

    Feat: Add HibernateUtil to connect to the database via application.properties

---
+++ src/main/java/com/example/onlinestore/util/HibernateUtil.java
@@ -0,0 +1,56 @@
+package com.example.onlinestore.util;
+
+import com.example.onlinestore.entity.Order;
+import com.example.onlinestore.entity.Product;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Properties properties = new Properties();
+            FileInputStream fis = new FileInputStream("src/main/resources/application.properties");
+            properties.load(fis);
+            fis.close();
+
+            // Создание конфигурации Hibernate
+            Configuration configuration = new Configuration();
+
+            // Добавление свойств в конфигурацию
+            configuration.setProperty("hibernate.connection.driver_class", properties.getProperty("hibernate.connection.driver_class"));
+            configuration.setProperty("hibernate.connection.url", properties.getProperty("hibernate.connection.url"));
+            configuration.setProperty("hibernate.connection.username", properties.getProperty("hibernate.connection.username"));
+            configuration.setProperty("hibernate.connection.password", properties.getProperty("hibernate.connection.password"));
+            configuration.setProperty("hibernate.dialect", properties.getProperty("hibernate.dialect"));
+            configuration.setProperty("hibernate.hbm2ddl.auto", properties.getProperty("hibernate.hbm2ddl.auto"));
+            configuration.setProperty("hibernate.show_sql", properties.getProperty("hibernate.show_sql"));
+            configuration.setProperty("hibernate.format_sql", properties.getProperty("hibernate.format_sql"));
+
+            // Добавление сущностей
+            configuration.addAnnotatedClass(Product.class);
+            configuration.addAnnotatedClass(Order.class);
+            // добавляйте другие сущности здесь
+
+            // Построение SessionFactory
+            return configuration.buildSessionFactory();
+        } catch (IOException ex) {
+            // Логирование ошибки
+            System.err.println("Failed to load application.properties" + ex);
+            throw new ExceptionInInitializerError(ex);
+        } catch (Throwable ex) {
+            // Логирование ошибки
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
